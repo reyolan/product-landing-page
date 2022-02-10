e66855b6267f84ea49d0343b6ff5eac8a2b7e830
reyolan <reynardolan@gmail.com>
2022-02-10 13:06:57 +0800

    finish initial style of nav

---
+++ index.js
@@ -0,0 +1,30 @@
+// for user story #12 --> pupunta sa dynamic page
+// ipapakita talaga yung ininput the e-mail
+// Lagay mo thank you for subscribing
+// basta magawa yung pagpasa sa data papunta sa ibang page
+
+(function () {
+	var queryString = widnow.location.search;
+	var href = window.location.href;
+	var parameters = new URLSearchParams(queryString);
+	var age = parameters.get("age");
+	var name = parameters.get("name");
+	var gender = parameters.get("gender");
+
+	console.log("age", age, "name", name, "gender", gender);
+
+	var domName = document.querySelector("[data-name]");
+	var domAge = document.querySelector("[data-age]");
+	var domGender = document.querySelector("[data-gender");
+
+	currentUrl.textContent = "current URL: " + href;
+
+	if (name && age && gender) {
+		domName.textContent = "Name: " + name;
+		domAge.textContent = "Age: " + age;
+		domGender.textContent = "Gender: " + gender;
+	}
+});
+
+// email is submitted to a dynamic page (challenge for the improvement)
+// I guess lagay tayo na E-mail submitted, redirecting to previous page in 5 4 3 2 1 seconds (?)
